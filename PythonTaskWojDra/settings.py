08f0ff2b1b2ee0b20720771f10877e3afd1ae534
wojciechdrazkiewicz <wojciech.drazkiewicz@gogomedia.pl>
2022-12-23 07:24:18 +0100

    Add environment variables and requirements

--- PythonTaskWojDra/settings.py
+++ PythonTaskWojDra/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -21,15 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ec1mb9kt_$4i$w&+uc0ip$8jkjyg+6@bl4_p4f61w@zjsa&dg$'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = [
     "*"
 ]
-
 
 # Application definition
 
